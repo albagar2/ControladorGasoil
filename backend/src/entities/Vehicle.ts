197429777c734b50d6ee9f9dcacffa18da5a794d
albagar2 <baciapez@gmail.com>
2026-03-09 19:52:08 +0100

    feat cambios en el formulario de coches

--- backend/src/entities/Vehicle.ts
+++ backend/src/entities/Vehicle.ts
@@ -18,30 +18,30 @@ export class Vehicle {
     @Column()
     modelo!: string;
 
-    @Column({ type: 'enum', enum: ['Gasolina', 'Diesel', 'Eléctrico', 'Híbrido', 'GLP', 'GNC'] })
+    @Column({ type: 'enum', enum: ['Gasolina', 'Diesel', 'Eléctrico', 'Híbrido', 'GLP', 'GNC'], default: 'Diesel' })
     combustible!: CombustibleType;
 
     @Column({ type: 'enum', enum: ['0', 'ECO', 'C', 'B'], default: 'C' })
     distintivo!: DistintivoType;
 
     // Seguro (Flattened)
-    @Column({ name: 'seguro_compañia' })
+    @Column({ name: 'seguro_compañia', nullable: true })
     seguroCompañia!: string;
 
-    @Column({ name: 'seguro_numero_poliza' })
+    @Column({ name: 'seguro_numero_poliza', nullable: true })
     seguroNumeroPoliza!: string;
 
-    @Column({ type: 'date', name: 'seguro_fecha_vencimiento' })
+    @Column({ type: 'date', name: 'seguro_fecha_vencimiento', nullable: true })
     seguroFechaVencimiento!: Date;
 
-    @Column({ type: 'enum', enum: ['Terceros', 'Terceros ampliado', 'Todo riesgo', 'Todo riesgo con franquicia'], name: 'seguro_cobertura' })
+    @Column({ type: 'enum', enum: ['Terceros', 'Terceros ampliado', 'Todo riesgo', 'Todo riesgo con franquicia'], name: 'seguro_cobertura', nullable: true })
     seguroCobertura!: CoberturaType;
 
     // ITV (Flattened)
     @Column({ type: 'enum', enum: ['Pasada', 'Pendiente', 'Caducada', 'Favorable', 'Desfavorable'], default: 'Pendiente', name: 'itv_estado' })
     itvEstado!: ItvEstadoType;
 
-    @Column({ type: 'date', name: 'itv_fecha_caducidad' })
+    @Column({ type: 'date', name: 'itv_fecha_caducidad', nullable: true })
     itvFechaCaducidad!: Date;
 
     @Column({ default: 0, name: 'itv_kilometraje' })
