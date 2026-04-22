34ddcb0f6e4df615b56f408b6214020d7d272aed
albagar2 <baciapez@gmail.com>
2026-04-22 13:18:43 +0200

    arreglos

--- backend/src/middleware/rate-limit.middleware.ts
+++ backend/src/middleware/rate-limit.middleware.ts
@@ -1,5 +1,4 @@
-// @ts-ignore
-import { rateLimit } from 'express-rate-limit';
+import rateLimit from 'express-rate-limit';
 
 export const globalLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
